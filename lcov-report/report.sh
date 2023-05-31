247a094cc1505d9d2ad0c6526f1c2416e07e89f5
20niship <idcnishimiya5378@gmail.com>
2023-05-31 21:51:04 +0900

    doxygenとCoverageレポート

--- lcov-report/report.sh
+++ lcov-report/report.sh
@@ -2,7 +2,16 @@
 
 # repo環境変数をcloneする
 echo "cloning repo : $repo"
+export GIT_SSH_COMMAND="ssh -i /root/.ssh/id_rsa -o StrictHostKeyChecking=no"
 git clone $repo /src
+
+# git cloneが失敗したら終了
+if [ $? -ne 0 ]; then
+  echo "git clone failed exitting......"
+  sleep 100000
+  exit 1
+fi
+
 cd /src
 
 git submodule update --init --recursive
